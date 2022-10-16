d763411c861c62048518628a3053d6462b2b6ec6
ylpitseleh <lyla02217@gmail.com>
2022-10-16 18:14:40 +0900

    Modify 66.PlusOne

--- src/leetcode/easy/_66_PlusOne.java
+++ src/leetcode/easy/_66_PlusOne.java
@@ -13,34 +13,45 @@ public class _66_PlusOne {
 //        int[] digits = {1, 2, 3};
 //        int[] digits = {9};
 //        int[] digits = {9, 9, 9, 9};
-        int[] digits = {9, 8, 7, 6, 5, 4, 3, 2, 1, 0};
+//        int[] digits = {9, 8, 7, 6, 5, 4, 3, 2, 1, 0};
 //        int[] digits = {1, 9};
-//        int[] digits = {4, 3, 2, 1};
+        int[] digits = {4, 3, 2, 1};
 
-        ArrayList<Integer> al = new ArrayList<>();
-        boolean addFlag = true; // 더해야하는지 여부
-        boolean allNine = true; // 모든 원소가 9인지 => 자릿수 + 1 해줘야 함
-        for (int i=digits.length-1; i>=0; i--) {
-            if (digits[i] == 9 && addFlag) {
-                al.add(0);
-                continue;
+//        ArrayList<Integer> al = new ArrayList<>();
+//        boolean addFlag = true; // 더해야하는지 여부
+//        boolean allNine = true; // 모든 원소가 9인지 => 자릿수 + 1 해줘야 함
+//        for (int i=digits.length-1; i>=0; i--) {
+//            if (digits[i] == 9 && addFlag) {
+//                al.add(0);
+//                continue;
+//            }
+//            else {
+//                allNine = false;
+//            }
+//            if (addFlag) {
+//                al.add(digits[i] + 1);
+//                addFlag = false;
+//            }
+//            else {
+//                al.add(digits[i]);
+//            }
+//        }
+//        if (allNine) {
+//            al.add(1);
+//        }
+//        Collections.reverse(al);
+//        return al.stream().mapToInt(Integer::intValue).toArray();
+
+        for(int i=digits.length-1; i>=0; i--) {
+            if (digits[i] != 9) {
+                digits[i]++;
+                return digits;
             }
-            else {
-                allNine = false;
-            }
-            if (addFlag) {
-                al.add(digits[i] + 1);
-                addFlag = false;
-            }
-            else {
-                al.add(digits[i]);
-            }
+            digits[i] = 0;
         }
-        if (allNine) {
-            al.add(1);
-        }
-        Collections.reverse(al);
-        return al.stream().mapToInt(Integer::intValue).toArray();
+        int[] newDigits = new int[digits.length + 1];
+        newDigits[0] = 1; // 초기화되면 어차피 0이니까 뒤에 digits로 채울 필요 없음
+        return newDigits;
     }
 
 }
