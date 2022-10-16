e01d1f402913e81af3989febf42d406285899243
ylpitseleh <lyla02217@gmail.com>
2022-10-16 14:20:23 +0900

    Modify 9.PalindromeNumber 2nd try

--- src/leetcode/easy/_9_PalindromeNumber.java
+++ src/leetcode/easy/_9_PalindromeNumber.java
@@ -1,5 +1,7 @@
 package leetcode.easy;
 
+import java.util.Stack;
+
 public class _9_PalindromeNumber {
 
     public static void main(String[] args) {
@@ -7,14 +9,23 @@ public class _9_PalindromeNumber {
     }
 
     private static boolean solve() {
-        int x = -121;
+        int x = 10;
+//        String s = Integer.toString(x);
+//        int idx = s.length()-1;
+//        for (int i=0; i<=s.length()/2-1; i++) {
+//            if (s.charAt(i) != s.charAt(i+idx)) {
+//                return false;
+//            }
+//            idx -= 2;
+//        }
+
+        Stack<Character> st = new Stack();
         String s = Integer.toString(x);
-        int idx = s.length()-1;
-        for (int i=0; i<=s.length()/2-1; i++) {
-            if (s.charAt(i) != s.charAt(i+idx)) {
+        for(char c : s.toCharArray())
+            st.push(c);
+        for(int i=s.length()-1; i>=0; i--) {
+            if (st.pop() != s.charAt(s.length() - 1 - i))
                 return false;
-            }
-            idx -= 2;
         }
         return true;
     }
