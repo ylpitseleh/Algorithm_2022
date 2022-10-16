51d3d42cf5146bb4a19b971455f10572e42e0d67
ylpitseleh <lyla02217@gmail.com>
2022-10-16 16:06:53 +0900

    Modify 20.ValidParentheses

--- src/leetcode/easy/_20_ValidParentheses.java
+++ src/leetcode/easy/_20_ValidParentheses.java
@@ -9,19 +9,33 @@ public class _20_ValidParentheses {
     }
 
     private static boolean solve() {
-        String s = "(){";
+        String s = "()";
+//        Stack<Character> st = new Stack<>();
+//        if (s.length() % 2 != 0) return false;
+//        for(char c : s.toCharArray()) {
+//            if (c == '(' || c == '[' || c == '{') {
+//                st.push(c);
+//                continue;
+//            }
+//            if (!st.isEmpty() && ((c == ')' && st.peek() == '(') || (c == '}' && st.peek() == '{') || ((c == ']' && st.peek() == '[')))) {
+//                st.pop();
+//                continue;
+//            }
+//            return false;
+//        }
+//        return st.isEmpty();
+
+        // 스택에 (가 나오면 )을 넣고 c가 )이 아니면 false
+        // 베끼다시피 풀긴했다만 나중에 기억못할듯
         Stack<Character> st = new Stack<>();
-        if (s.length() % 2 != 0) return false;
         for(char c : s.toCharArray()) {
-            if (c == '(' || c == '[' || c == '{') {
-                st.push(c);
-                continue;
-            }
-            if (!st.isEmpty() && ((c == ')' && st.peek() == '(') || (c == '}' && st.peek() == '{') || ((c == ']' && st.peek() == '[')))) {
-                st.pop();
-                continue;
-            }
-            return false;
+            if (c == '(')
+                st.push(')');
+            else if (c == '{')
+                st.push('}');
+            else if (c == '[')
+                st.push(']');
+            else if (st.isEmpty() || st.pop() != c) return false;
         }
         return st.isEmpty();
     }
