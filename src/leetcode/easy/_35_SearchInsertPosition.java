242361aa0f75b31fc802433075a7a30e1646bcae
ylpitseleh <lyla02217@gmail.com>
2022-10-16 16:51:41 +0900

    Modify 35.SearchInsertPosition

--- src/leetcode/easy/_35_SearchInsertPosition.java
+++ src/leetcode/easy/_35_SearchInsertPosition.java
@@ -10,20 +10,16 @@ public class _35_SearchInsertPosition {
         int[] nums = {1, 3, 5, 6};
 //        int target = 5; // 2
 //        int target = 2; // 1
-        int target = 7; // 4
+//        int target = 7; // 4
+        int target = 0; // 0
 
-        // 있으면 index, 없으면 들어갈 index Olog(n) 안으로
         int start = 0;
-        int end = nums.length - 1; // 3
+        int end = nums.length - 1;
         while (start <= end) {
             int mid = (start + end) / 2;
-
-            if (target == nums[mid])
-                return mid;
-            else if (target > nums[mid])
-                start = mid + 1;
-            else if (target < nums[mid])
-                end = mid - 1;
+            if (target == nums[mid]) return mid;
+            else if (target > nums[mid]) start = mid + 1;
+            else if (target < nums[mid]) end = mid - 1;
         }
         return start;
     }
