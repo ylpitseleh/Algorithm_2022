4cdf256a915ab623ba47192d553037f7bca72bd0
ylpitseleh <lyla02217@gmail.com>
2022-10-30 16:30:58 +0900

    Review 1.TwoSome

---
+++ src/leetcode/easy/revision.java
@@ -0,0 +1,28 @@
+package leetcode.easy;
+
+import java.util.HashMap;
+
+public class revision {
+    public static void main(String[] args) {
+        for (int i : solve()) {
+            System.out.println("i = " + i);
+        }
+    }
+
+    private static int[] solve() {
+        int[] nums = {2, 7, 11, 15};
+        int target = 9;
+//        int[] nums = {3, 2, 4};
+//        int target = 6;
+//        int[] nums = {3, 3};
+//        int target = 6;
+        HashMap<Integer, Integer> hm = new HashMap<>();
+        for(int i=0; i<nums.length; i++) {
+            if (hm.containsKey(target - nums[i])) {
+                return new int[]{hm.get(target - nums[i]), i};
+            }
+            hm.put(nums[i], i);
+        }
+        return null;
+    }
+}
