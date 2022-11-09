da541d8615422b88e8e1ae9e12819f6addd73237
ylpitseleh <lyla02217@gmail.com>
2022-11-09 20:44:31 +0900

    Review 13.RomanToInteger

--- src/leetcode/easy/revision.java
+++ src/leetcode/easy/revision.java
@@ -4,25 +4,39 @@ import java.util.HashMap;
 
 public class revision {
     public static void main(String[] args) {
-        for (int i : solve()) {
-            System.out.println("i = " + i);
-        }
+//        String s = "III";
+//        String s = "LVIII";
+        String s = "MCMXCIV";
+        System.out.println(romanToInt(s));
     }
 
-    private static int[] solve() {
-        int[] nums = {2, 7, 11, 15};
-        int target = 9;
-//        int[] nums = {3, 2, 4};
-//        int target = 6;
-//        int[] nums = {3, 3};
-//        int target = 6;
-        HashMap<Integer, Integer> hm = new HashMap<>();
-        for(int i=0; i<nums.length; i++) {
-            if (hm.containsKey(target - nums[i])) {
-                return new int[]{hm.get(target - nums[i]), i};
+    private static int romanToInt(String s) {
+        HashMap<String, Integer> hm = new HashMap<>();
+        hm.put("I", 1);
+        hm.put("V", 5);
+        hm.put("X", 10);
+        hm.put("L", 50);
+        hm.put("C", 100);
+        hm.put("D", 500);
+        hm.put("M", 1000);
+        hm.put("IV", 4);
+        hm.put("IX", 9);
+        hm.put("XL", 40);
+        hm.put("XC", 90);
+        hm.put("CD", 400);
+        hm.put("CM", 900);
+        int answer = 0;
+        for (int i=0; i<s.length(); i++) {
+            if (i < s.length()-1) {
+                String sub = s.substring(i, i+2);
+                if (hm.containsKey(sub)) {
+                    answer += hm.get(sub);
+                    i++;
+                    continue;
+                }
             }
-            hm.put(nums[i], i);
+            answer += hm.get(s.substring(i, i+1));
         }
-        return null;
+        return answer;
     }
 }
