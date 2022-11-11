82699a2804e4b2e679eb74acb3adf43be5ba5092
ylpitseleh <lyla02217@gmail.com>
2022-11-11 19:10:47 +0900

    Review 14.LongestCommonPrefix

--- src/leetcode/easy/revision.java
+++ src/leetcode/easy/revision.java
@@ -4,38 +4,24 @@ import java.util.HashMap;
 
 public class revision {
     public static void main(String[] args) {
-//        String s = "III";
-//        String s = "LVIII";
-        String s = "MCMXCIV";
-        System.out.println(romanToInt(s));
+        String[] strs = {"flower","flow","flight"};
+//        String[] strs = {"dog","racecar","car"};
+//        String[] strs = {"a"};
+//        String[] strs = {"reflower","flow","flight"};
+//        String[] strs = {"ab","a"};
+//        String[] strs = {"aa", "aa"};
+        System.out.println("longestCommonPrefix() = " + longestCommonPrefix(strs));
     }
 
-    private static int romanToInt(String s) {
-        HashMap<String, Integer> hm = new HashMap<>();
-        hm.put("I", 1);
-        hm.put("V", 5);
-        hm.put("X", 10);
-        hm.put("L", 50);
-        hm.put("C", 100);
-        hm.put("D", 500);
-        hm.put("M", 1000);
-        hm.put("IV", 4);
-        hm.put("IX", 9);
-        hm.put("XL", 40);
-        hm.put("XC", 90);
-        hm.put("CD", 400);
-        hm.put("CM", 900);
-        int answer = 0;
-        for (int i=0; i<s.length(); i++) {
-            if (i < s.length()-1) {
-                String sub = s.substring(i, i+2);
-                if (hm.containsKey(sub)) {
-                    answer += hm.get(sub);
-                    i++;
-                    continue;
+    private static String longestCommonPrefix(String[] strs) {
+        String answer = "";
+        for (int i=0; i<strs[0].length(); i++) {
+            for (int j=1; j<strs.length; j++) {
+                if (strs[j-1].length() <= i || strs[j].length() <= i || strs[j].charAt(i) != strs[j-1].charAt(i)) {
+                    return answer;
                 }
             }
-            answer += hm.get(s.substring(i, i+1));
+            answer += strs[0].substring(i, i+1);
         }
         return answer;
     }
