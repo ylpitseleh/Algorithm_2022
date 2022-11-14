2789f44a82f110c55257294014bb13611e9f1bc1
ylpitseleh <lyla02217@gmail.com>
2022-11-14 19:31:48 +0900

    Review 20.ValidParentheses

--- src/leetcode/easy/revision.java
+++ src/leetcode/easy/revision.java
@@ -1,26 +1,30 @@
 package leetcode.easy;
 
+import java.util.Stack;
+
 public class revision {
     public static void main(String[] args) {
-        String[] strs = {"flower","flow","flight"};
-//        String[] strs = {"dog","racecar","car"};
-//        String[] strs = {"a"};
-//        String[] strs = {"reflower","flow","flight"};
-//        String[] strs = {"ab","a"};
-//        String[] strs = {"aa", "aa"};
-        System.out.println("longestCommonPrefix() = " + longestCommonPrefix(strs));
+//        String s = "()";
+//        String s = "()[]{}";
+//        String s = "(]";
+//        String s = "({[)";
+//        String s = "]";
+        String s = "(])";
+        System.out.println("isValid() = " + isValid(s));
     }
 
-    private static String longestCommonPrefix(String[] strs) {
-        String answer = "";
-        for (int i=0; i<strs[0].length(); i++) {
-            for (int j=1; j<strs.length; j++) {
-                if (strs[j-1].length() <= i || strs[j].length() <= i || strs[j].charAt(i) != strs[j-1].charAt(i)) {
-                    return answer;
-                }
+    private static boolean isValid(String s) {
+        Stack<Character> st = new Stack<>();
+        for (int i=0; i<s.length(); i++) {
+            char c = s.charAt(i);
+            if (c == '(' || c == '{' || c == '[') {
+                st.add(c);
+                continue;
             }
-            answer += strs[0].substring(i, i+1);
+            else if (st.isEmpty()) return false;
+            if ((st.peek() == '(' && c == ')') || (st.peek() == '[' && c == ']') || (st.peek() == '{' && c == '}')) st.pop();
+            else return false;
         }
-        return answer;
+        return st.isEmpty()? true: false;
     }
 }
