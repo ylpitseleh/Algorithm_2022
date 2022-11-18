cebf77f51a47aebbc9a236b6dbffb1dd49cc5623
ylpitseleh <lyla02217@gmail.com>
2022-11-18 15:15:06 +0900

    Review 35.SearchInsertPosition

--- src/leetcode/easy/revision.java
+++ src/leetcode/easy/revision.java
@@ -2,19 +2,28 @@ package leetcode.easy;
 
 public class revision {
     public static void main(String[] args) {
-        int[] nums = {1,1,2};
-//        int[] nums = {0,0,1,1,1,2,2,3,3,4};
-        System.out.println("removeDuplicates() = " + removeDuplicates(nums));
+        int[] nums = {1,3,5,6};
+//        int target = 5;
+//        int target = 2;
+        int target = 7;
+        System.out.println("searchInsert() = " + searchInsert(nums, target));
         
     }
 
-    private static int removeDuplicates(int[] nums) {
-        int idx = 1;
-        for (int i=1; i<nums.length; i++) {
-            if (nums[i] > nums[idx-1]) {
-                nums[idx++] = nums[i];
+    private static int searchInsert (int[] nums, int target) {
+        int start = 0;
+        int end = nums.length - 1;
+
+        while (start <= end) {
+            int mid = (start + end) / 2;
+            if (nums[mid] == target) return mid;
+            else if (nums[mid] > target) {
+                end = mid - 1;
+            }
+            else {
+                start = mid + 1;
             }
         }
-        return idx;
+        return start;
     }
 }
